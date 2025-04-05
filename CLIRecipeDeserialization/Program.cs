f0454248baa0aa33e629cce7377d5c0f8dd183e1
Syndaryl <emily.smirle@gmail.com>
2025-04-05 18:19:17 -0400

    Able to load a minecraft:crafting_shaped recipe without error and print out information

--- CLIRecipeDeserialization/Program.cs
+++ CLIRecipeDeserialization/Program.cs
@@ -6,26 +6,48 @@ using Syndaryl.Minecraft.JSON;
 
 
 // demo of a Recipe object
-var recipe = new Recipe
-{
-    Type = "minecraft:crafting_shaped",
-    Category = "misc",
-    Key = new Dictionary<string, IRecipeComponent>()
+var recipe = new Recipe(type: "minecraft:crafting_shaped", category: "misc",
+    key: new Dictionary<string, RecipeComponent>()
     {
         { "A", RecipeItem.CreateInstance("minecraft:andesite") },
         { "B", RecipeTag.CreateInstance("c:nuggets/iron") }
-    },
-    Pattern = new()
+    }, pattern: new()
     {
         "BA",
         "AB"
-    },
-    Result = new()
-    {
-        RecipeItem.CreateInstance (
-            "minecraft:andesite_alloy",
-            1
-        )
-    }
-};
+    }, result: RecipeItem.CreateInstance(
+        "minecraft:andesite_alloy",
+        1
+    ), name:"recipe:test.nojson");
 
+List<Recipe> cliRecipes = new List<Recipe>(args.Length);
+cliRecipes.Add(recipe);
+
+if (args.Length > 0)
+{
+
+    // if there are arguments, treat them as file paths
+    foreach (var arg in args)
+    {
+        using (var stream = File.OpenRead(arg))
+            try
+            {
+                var r = Recipe.ReadFromStream(stream);
+                r.Name = arg;
+                cliRecipes.Add(r);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading recipe from {arg}: {ex.Message}");
+            }
+    }
+}
+    
+    
+Console.WriteLine("\r\n\r\nCLI Recipe Deserialization\r\n");
+foreach (var cliRecipe in cliRecipes)
+{
+    Console.WriteLine(cliRecipe);
+    Console.WriteLine();
+}
+Console.ReadKey();
--- Minecraft.JSON.Recipe/Recipe.cs
+++ Minecraft.JSON.Recipe/Recipe.cs
@@ -1,37 +1,96 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Syndaryl.Minecraft.JSON
 {
-    public record Recipe
+    public class Recipe
     {
+        [JsonIgnore]
+        public string Name { get; set; }
 
-        [Required]
+        [Required, JsonPropertyName("type")]
         public string Type { get; set; } = "minecraft:crafting";
         
-        [Required]
+        [JsonPropertyName("category")]
         public string Category { get; set; } = "misc";
         
-        [Required]
-        public Dictionary<string, IRecipeComponent> Key { get; set; } = new Dictionary<string, IRecipeComponent>();
+        [JsonPropertyName("key")]
+        public Dictionary<string, RecipeComponent> Key { get; set; } = new Dictionary<string, RecipeComponent>();
 
-        [Required]
+        [JsonPropertyName("pattern")]
         public List<string> Pattern { get; set; } = new List<string>();
 
-        [Required]
-        public List<RecipeItem> Result { get; set; } = new List<RecipeItem>();
+        [Required, JsonPropertyName("result")]
+        public RecipeComponent Result { get; set; }
 
         public override string ToString()
         {
             string keyString = string.Join(", ", Key.Select(kvp => $"{kvp.Key}: {kvp.Value.Id}"));
             string patternString = string.Join(", ", Pattern);
-            string resultString = string.Join(", ", Result.Select(r => r.ToString()));
-            return $"Type: {Type}, Category: {Category}, \r\n\tKey: {keyString}, \r\n\tPattern: {patternString}, \r\n\tResult: {resultString}";
+            string resultString = Result.ToString();
+            return $"Name: {Name}, Type: {Type}, Category: {Category}, \r\n\tKey: {keyString}, \r\n\tPattern: {patternString}, \r\n\tResult: {resultString}";
+        }
+
+        public Recipe(string type, string category, Dictionary<string, RecipeComponent> key, List<string> pattern, RecipeItem result, string name="recipe:empty")
+        {
+            Type = type;
+            Category = category;
+            Key = key;
+            Pattern = pattern;
+            Result = result;
+            Name = name;
+        }
+
+        public static Recipe Empty => new(type: "minecraft:crafting", category: "misc",
+            key: new Dictionary<string, RecipeComponent>(), pattern: new List<string>(), result: RecipeItem.Empty);
+
+        public Recipe()
+        {
+            Type = Recipe.Empty.Type;
+            Category = Recipe.Empty.Category;
+            Key = Recipe.Empty.Key;
+            Pattern = Recipe.Empty.Pattern;
+            Result = Recipe.Empty.Result;
+            Name = "recipe:empty";
+        }
+
+        private static readonly JsonSerializerOptions options = new()
+        {
+            PropertyNameCaseInsensitive = true,
+            Converters =
+            {
+                new System.Text.Json.Serialization.JsonStringEnumConverter(),
+                new RecipeComponentConverter()
+            },
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.KebabCaseLower
+        };
+
+        public static Recipe ReadFromStream(Stream stream)
+        {
+            ArgumentNullException.ThrowIfNull(stream, nameof(stream));
+            
+            Recipe result;
+            try
+            {
+                result = JsonSerializer.Deserialize<Recipe>(stream, options) ?? Recipe.Empty;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            if (result == Recipe.Empty)
+            {
+                throw new InvalidOperationException("Deserialization failed.");
+            }
+
+        
+            return result;
         }
     }
-}
 
-namespace Syndaryl.Minecraft.JSON
-{
     public interface IRecipeComponent
     {
         public string Id { get; set; }
@@ -40,11 +99,15 @@ namespace Syndaryl.Minecraft.JSON
         public int Count { get; set; }
     }
 
-    public class RecipeItem : IRecipeComponent
+    public  class RecipeComponent : IRecipeComponent
     {
-        internal static readonly RecipeItem Empty = RecipeItem.CreateInstance("minecraft:air", 1);
+        public RecipeComponent()
+        {
 
-        private RecipeItem(string id)
+        }
+
+        [JsonConstructor]
+        public RecipeComponent(string id, int count = 1)
         {
             if (string.IsNullOrWhiteSpace(id))
                 throw new ArgumentException("Value cannot be null or whitespace.", nameof(id));
@@ -54,37 +117,54 @@ namespace Syndaryl.Minecraft.JSON
             string[] parts = id.Split(':');
             Namespace = parts[0];
             Name = parts[1];
-            Count = 1;
+            Count = count;
         }
-
-        private RecipeItem(string id, int count) : this(id)
+        public static readonly RecipeComponent Empty = new RecipeComponent()
         {
-            if (count < 1)
-                throw new ArgumentOutOfRangeException(nameof(count), "Count must be greater than 0.");
-            {
+            Id = "minecraft:air",
+            Namespace = "minecraft",
+            Name = "air",
+            Count = 1
+        };
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = string.Empty;
 
-                Count = count;
-            }
-        }
-
-        public static RecipeItem CreateInstance(string id)
+        [JsonPropertyName("namespace")]
+        public string Namespace { get; set; } = string.Empty;
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+        [JsonPropertyName("count")]
+        public int Count { get; set; } = 1;
+        public static RecipeComponent CreateInstance(string id, int count=1)
         {
-            return new RecipeItem(id);
+            return new RecipeComponent(id, count);
         }
 
-        public static RecipeItem CreateInstance(string id, int count)
+        public override string ToString()
+        {
+            return $"RecipeComponent(Id: {Id}, Namespace: {Namespace}, Name: {Name}, Count: {Count})";
+        }
+    }
+
+    public class RecipeItem : RecipeComponent
+    {
+        internal new static readonly RecipeItem Empty = RecipeItem.CreateInstance("minecraft:air", 1);
+        
+        //[JsonPropertyName("item")]
+        //public new string Id { get; set; } = string.Empty;
+        
+
+        public RecipeItem(string id) : base(id)
+        {
+        }
+
+        public RecipeItem(string id, int count) : base(id, count)
+        {
+        }
+
+        public static RecipeItem CreateInstance(string id, int count=1)
         {
             return new RecipeItem(id, count);
-        }
-
-        public string Id { get; set; }
-        public string Namespace { get; set; }
-        public string Name { get; set; }
-
-        public int Count
-        {
-            get => 1;
-            set { }
         }
 
         public override string ToString()
@@ -93,39 +173,97 @@ namespace Syndaryl.Minecraft.JSON
         }
     }
 
-    public class RecipeTag : IRecipeComponent
+    public class RecipeTag : RecipeComponent
     {
-        private RecipeTag(string id)
+
+        private RecipeTag(string id) : base(id)
         {
-            if (string.IsNullOrWhiteSpace(id))
-                throw new ArgumentException("Value cannot be null or whitespace.", nameof(id));
-            if (!id.Contains(':'))
-                throw new ArgumentException("Value must contain a namespace and name.", nameof(id));
-            Id = id;
-            string[] parts = id.Split(':');
-            Namespace = parts[0];
-            Name = parts[1];
-            Count = 1;
+        }
+        private RecipeTag(string id, int count) : base(id, count)
+        {
+        }
+        //[JsonPropertyName("tag")]
+        //public new string Id { get; set; } = string.Empty;
+
+        public static RecipeTag CreateInstance(string id, int count=1)
+        {
+            var result = new RecipeTag(id)
+            {
+                Count = count
+            };
+            return result;
         }
 
-        public static RecipeTag CreateInstance(string id)
-        {
-            return new RecipeTag(id);
-        }
+        public new static readonly RecipeItem Empty = RecipeItem.CreateInstance("#minecraft:invalid", 1);
 
-        public string Id { get; set; }
-        public string Namespace { get; set; }
-        public string Name { get; set; }
-
-        public int Count
-        {
-            get => 1;
-            set { }
-        }
-        
         public override string ToString()
         {
             return $"RecipeTag(Id: {Id}, Namespace: {Namespace}, Name: {Name}, Count: {Count})";
         }
     }
+
+    public class RecipeComponentConverter : JsonConverter<RecipeComponent>
+    {
+        public override RecipeComponent Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using var doc = JsonDocument.ParseValue(ref reader);
+            var root = doc.RootElement;
+            var count = 1;
+            if ( root.TryGetProperty("count", out var countElement) )
+                count = countElement.GetInt32();
+            var id = string.Empty;
+            // Check which field is present
+            if (root.TryGetProperty("item", out var itemProp))
+            { 
+                id = itemProp.GetString() ?? "";
+                Console.WriteLine($"Item: {id}");
+                return RecipeItem.CreateInstance(
+                    id,
+                    count
+                );
+            }
+            else if (root.TryGetProperty("tag", out var tagProp))
+            {
+                id = tagProp.GetString() ?? "";
+                Console.WriteLine($"Tag: {id}");
+                return RecipeTag.CreateInstance(
+                    id,
+                    count
+                );
+            }
+            else if (root.TryGetProperty("id", out var idProp))
+            {
+                id = idProp.GetString() ?? "";
+                Console.WriteLine($"Id: {id}");
+                return RecipeComponent.CreateInstance(
+                    id,
+                    count
+                );
+            }
+
+            throw new JsonException("Unknown RecipeComponent type");
+        }
+
+        public override void Write(Utf8JsonWriter writer, RecipeComponent value, JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            writer.WriteNumber("count", value.Count);
+
+            switch (value)
+            {
+                case RecipeItem:
+                    writer.WriteString("item", value.Id);
+                    break;
+                case RecipeTag:
+                    writer.WriteString("tag", value.Id);
+                    break;
+                default:
+                    writer.WriteString("id", value.Id);
+                    break;
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+
 }
